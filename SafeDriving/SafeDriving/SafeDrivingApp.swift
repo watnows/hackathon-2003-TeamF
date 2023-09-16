14d54c85fe300c583ad82f08000b6cd556e902b9
Nakagawa Shinichi <98950032+shinpochi2002@users.noreply.github.com>
2023-09-16 11:08:11 +0900

    firstcommit

---
+++ SafeDriving/SafeDriving/SafeDrivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeDrivingApp.swift
+//  SafeDriving
+//
+//  Created by 中川真一 on 2023/09/16.
+//
+
+import SwiftUI
+
+@main
+struct SafeDrivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
